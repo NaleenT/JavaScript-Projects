9f2ee5151c0da0e648d84a4611212d0096159fd3
NaleenT <120801571+NaleenT@users.noreply.github.com>
2024-01-25 19:40:28 -0700

    Add files via upload

---
+++ Project1_expressions_alert/JS/JavaScript_File.js
@@ -0,0 +1,16 @@
+var A = "Popup!" + "Click to exit";
+
+var Dog = "Jack", Dogdog = "Braiden", Dogdogdog = "Rex";
+
+document.write(3 + 3)
+
+window.alert(A)
+
+document.write("Cartmen said " + "\"You will respect my authority\"");
+
+function My_First_Function () {
+    var str = "This is the button text!";
+    document.getElementById("Button_Text")
+.innerHTML = str;
+}
+
