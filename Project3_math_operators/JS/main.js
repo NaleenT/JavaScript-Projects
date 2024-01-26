f95798a27df026d513b90c8797f66feac4ad5ba2
NaleenT <120801571+NaleenT@users.noreply.github.com>
2024-01-26 11:05:00 -0700

    Add files via upload

---
+++ Project3_math_operators/JS/main.js
@@ -0,0 +1,46 @@
+
+window.alert(Math.random());
+
+function Subtraction_Function() { //Write a function to change the text on a button
+    var subtraction = 8 - 5;
+    document.getElementById("Math").innerHTML = "8 - 5 = " + subtraction;
+}
+
+function addition_Function() { //Write a function to change the text on a button
+    var addition = 13 + 5;
+    document.getElementById("MathA").innerHTML = "13 + 5 = " + addition;
+}
+
+function muti_Function() { //Write a function to change the text on a button
+    var simple_Math = 20 * 2;
+    document.getElementById("MathM").innerHTML = "20 * 2 = " + simple_Math;
+}
+
+function division_Function() { //Write a function to change the text on a button
+    var simple_Math = 20 / 2;
+    document.getElementById("MathD").innerHTML = "20 / 2 = " + simple_Math;
+}
+
+function more_math() { //Write a function to change the text on a button
+    var simple_Math = (20 / 2) * (2 + 5) - 1;
+    document.getElementById("MathAll").innerHTML = "(20 / 2) * (2 + 5) -1 = " + simple_Math;
+}
+
+function MO_math() { //Write a function to change the text on a button
+    var simple_Math = 30 % 5;
+    document.getElementById("MathMO").innerHTML = "30 % 5 = " + simple_Math;
+}
+
+function negation_operator() { //Write a function to change the text on a button
+    var X = 5;
+    X--
+
+    document.getElementById("MathNO").innerHTML = -X;
+}
+
+function Decrement_operator() { //Write a function to change the text on a button
+    var X = 5;
+    X++
+    document.getElementById("MathDO").innerHTML = X;
+}
+
