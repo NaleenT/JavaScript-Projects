64e9a1b9c433b2399642562f8f52b5e6e40949a2
NaleenT <120801571+NaleenT@users.noreply.github.com>
2024-02-06 13:27:26 -0700

    Add files via upload

--- Project10_loops_arrays/JS/main.js
+++ Project10_loops_arrays/JS/main.js
@@ -69,7 +69,7 @@ document.getElementById("Car_Object").innerHTML = car.description();
 let item = "";
 for (let i = 0; i < 10; i++) {
   if (i === 3) { break; }
-  text += "The number is " + i + "<br>";
+  item += "The number is " + i + "<br>";
 }
 
 document.getElementById("Break").innerHTML = item;
@@ -79,8 +79,36 @@ function TestC() {
     let item2 = "";
     for (let i = 0; i < 10; i++) {
     if (i === 3) { continue; }
-    text += "The number is " + i + "<br>";
+    item2 += "The number is " + i + "<br>";
     }
     document.getElementById("Cont").innerHTML = item2;
 }
 
+function Color_Function() {
+    var Color_Output;
+    var Colors = document.getElementById("Color_Input").value;
+    var Color_String = " is a great color!";
+    switch(Colors) {
+        case "Red":
+            Color_Output = "Red" + Color_String;
+        break;
+        case "Green":
+            Color_Output = "Green" + Color_String;
+        break;
+        case "Yellow":
+            Color_Output = "Yellow" + Color_String;
+        break;
+        case "Blue":
+            Color_Output = "Blue" + Color_String;
+        break;
+        case "Pink":
+            Color_Output = "Pink" + Color_String;
+        break;
+        case "Purple":
+            Color_Output = "Purple" + Color_String;
+        break;
+        default:
+        Color_Output = "Please enter a color exactly as written above";        
+    }
+    document.getElementById("Output").innerHTML = Color_Output;
+}
